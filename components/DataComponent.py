28b49395226697bce3946dbfdde9b193913eaf9f
John Arban Lewis <johnarban@users.noreply.github.com>
2023-08-06 17:45:23 -0400

    show hubble and age with student data

--- components/DataComponent.py
+++ components/DataComponent.py
@@ -28,7 +28,12 @@ def DataSummary(data = None, student_id = None, on_student_id = None):
     ClassPlot(data.value, on_click=on_plot_click, select_on = 'student_id', selected = student_id)
     
 
-
+from math import nan
+def get_slope(x, y):
+    # slope through origin
+    return nan if (sum(x**2) == 0) else (sum(x*y)/sum(x**2))
+def slope2age(h0):
+    return 977.79222 / h0 # age of universe in Gyr
 
 @solara.component
 def StudentData(dataframe = None, id_col = 'student_id',  sid = None, cols_to_display = None, on_sid = None, allow_id_set = True):
@@ -55,11 +60,21 @@ def StudentData(dataframe = None, id_col = 'student_id',  sid = None, cols_to_di
             on_sid(int(value))
     
     with solara.Column(gap="0px"):
-        if allow_id_set:
-            # val = str(sid.value)
-            solara.InputText(label="Student ID",  value = str(sid), on_value=on_value)
-        else:
-            solara.Markdown(f"**Student {sid}**")
+        with solara.Columns([1,1]):
+            with solara.Column():
+                if allow_id_set:
+                    # val = str(sid.value)
+                    solara.InputText(label="Student ID",  value = str(sid), on_value=on_value)
+                else:
+                    solara.Markdown(f"**Student {sid}**")
+            with solara.Column():
+                #hubble's constant
+                # age of universe
+                h0 = get_slope(single_student_df['est_dist_value'], single_student_df['velocity_value'])
+                age = slope2age(h0)
+                solara.Markdown(f"**Hubble Constant**: {h0:.1f} km/s/Mpc")
+                solara.Markdown(f"**Age of Universe**: {age:.1f} Gyr")
+                
         
         if cols_to_display is None:
             cols_to_display = single_student_df.columns
