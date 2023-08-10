25eb62bead6c12ed64b606a8da06ffc562a1658f
John Arban Lewis <johnarban@users.noreply.github.com>
2023-08-10 12:13:42 -0400

    update responses

--- components/ResponsesComponents.py
+++ components/ResponsesComponents.py
@@ -1,6 +1,9 @@
 import solara
 from pandas import DataFrame
 
+# from solara.components.dataframe import HistogramCard
+
+import solara.express as px
 
 @solara.component
 def QuestionSummary(df = None, sid = None):
@@ -12,17 +15,38 @@ def QuestionSummary(df = None, sid = None):
     else:
         solara.DataFrame(df)
         
-        
+
 
 @solara.component
 def StudentQuestion(df = None):
     """
     Show the responses for a single student
     """
+    
     if isinstance(df, solara.Reactive):
-        solara.DataFrame(df.value)
+        # solara.DataFrame(df.value)
+        table = df.value
     else:
-        solara.DataFrame(df)
+        # solara.DataFrame(df)
+        table = df
+        
+    # table is pandas dataframe with columns: question and score
+    # solara.DataFrame(table)
+    #mc_questions rows with value where len(value.split('.'))==3
+    mc_questions = table[table.question.apply(lambda x: len(str(x).split('.'))==3)]
+    #fr_questions rows with value where len(value.split('.'))==2
+    fr_questions = table[table.question.apply(lambda x: len(str(x).split('.'))==2)]
+    
+    # multiple choice questions
+    with solara.Columns([1,1]):
+        with solara.Column():
+            solara.Markdown('**Multiple Choice**')
+            # solara.DataFrame(mc_questions)
+            px.histogram(mc_questions.dropna(), 'score', custom_data= ['question'],)
+            # solara.FigurePlotly(fig.)
+        with solara.Column():
+            solara.Markdown('**Free Response**')
+            solara.DataFrame(fr_questions)
         
         
 @solara.component
@@ -45,12 +69,12 @@ def IndividualStudentResponses(roster, sid=None):
         for i, row in questions.iterrows(): 
             with solara.lab.Tab(str(row['student_id'])):
                 solara.Markdown(f"**Student {row['student_id']}**")
-                df = DataFrame(row.drop('student_id'))
+                df = row.drop('student_id').rename('score').to_frame()
                 # copy the index to a column
                 df['question'] = df.index
                 # drop rows where question contains score or choice in string
                 df = df[~df['question'].str.contains('score|choice')]
-                StudentQuestion(df)
+                StudentQuestion(df[['question', 'score']])
 
 @solara.component
 def StudentQuestions(roster, sid = None):
