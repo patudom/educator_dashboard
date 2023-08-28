5b9099b90102066891750e4f4c86d2ccf01163ed
John Arban Lewis <johnarban@users.noreply.github.com>
2023-08-28 09:31:00 -0400

    place all mc and fr functionality in dedicated components

--- educator_dashboard/components/MultipleChoice.py
+++ educator_dashboard/components/MultipleChoice.py
@@ -1,12 +1,16 @@
 import solara
 
-from pandas import DataFrame, Series
+from pandas import DataFrame, Series, concat
 from ..database.Query import QueryCosmicDSApi as Query
 import plotly.express as px
 from .Collapsable import Collapsable
 
 @solara.component
 def MCSummaryPart(stage_qs):
+    """
+    stage_qs is a dictionary of questions and tries
+     = {q1: {tries:0, choice: 0, score: 0}...}
+    """
     
     quest = solara.use_reactive(None)
     
@@ -45,8 +49,21 @@ def MultipleChoiceSummary(roster):
 
 
 @solara.component
-def MultipleChoiceQuestionSingleStudent(mc_questions):
+def MultipleChoiceQuestionSingleStudent(roster, sid = None):
+    
+    if not isinstance(sid, solara.Reactive):
+        sid = solara.use_reactive(sid)
+
+    if sid.value is None:
+        return
+    
     dquest, set_dquest = solara.use_state(None)
+    
+    idx = roster.value.student_ids.index(sid.value)
+    mc_questions = roster.value.roster[idx]['story_state']['mc_scoring']
+    # {stage: {q1: {tries:0, choice: 0, score: 0}..., }
+    
+    
     
     def mc_cell_action(column, row_index):
         tag = mc_questions['tag'].iloc[row_index]
@@ -60,35 +77,38 @@ def MultipleChoiceQuestionSingleStudent(mc_questions):
         
     mc_cell_actions = [solara.CellAction('Show Question', icon='mdi-help-box', on_click=mc_cell_action)]
     
-    solara.Markdown('')
+    with solara.Row():
+        with solara.Column():
+            dflist = []
+            for stage, v in mc_questions.items():
+                df = DataFrame(v).T
+                df['stage'] = stage
+                df['key'] = df.index
+                df['question'] = [roster.value.question_keys()[k]['shorttext'] for k in df.key]
+                dflist.append(df)
 
-    df = DataFrame()
+                mc_df = concat(dflist,axis=0)
+                df = mc_df[['key', 'question','stage', 'tries','score']]
 
-    tag = mc_questions.tag
-    df['question'] = mc_questions.question[::3]
-    df['score'] = mc_questions['value'][tag.str.contains('score')].values
-    df['tries'] = mc_questions['value'][tag.str.contains('tries')].values
-    df['choice'] = mc_questions['value'][tag.str.contains('choice')].values
-    # df['tag'] = mc_questions['tag'][tag.str.contains('choice')].values
 
-    completed = sum(df.score.notna())
-    total = len(df)
-    points = sum(df.score.dropna().astype(int))
-    total_points = 10 * total
-    solara.Markdown("""
-                    ## Multiple Choice
-                    Student completed {} out of {} multiple choice questions </br> Multiple Choice Score: {}/{}
-                    """.format(completed, total, points, total_points))    
+                completed = sum(df.score.notna())
+                total = len(df)
+                points = sum(df.score.dropna().astype(int))
+                total_points = 10 * total
+                solara.Markdown("""
+                                ## Multiple Choice
+                                Student completed {} out of {} multiple choice questions </br> Multiple Choice Score: {}/{}
+                                """.format(completed, total, points, total_points))    
 
-    df_nona = df.dropna()
-    df_nona['tries'] = df_nona['tries'].astype(int)
-    fig = px.histogram(df_nona, 'tries', hover_data= ['question'], labels={'tries': "# of Tries"}, range_x=[-0.6,3.6], category_orders={'tries': [0,1,2,3,4]})
-    fig.update_xaxes(type='category')
-    solara.FigurePlotly(fig)
-    # solara.DataFrame(df.dropna())
-    
-    with Collapsable(header='Show Question Table'):
-        if dquest is not None:
-            solara.Markdown(f"**Question**: {dquest}")
-        solara.DataFrame(df, items_per_page=len(df), cell_actions=mc_cell_actions)
+                df_nona = df.dropna()
+                df_nona['tries'] = df_nona['tries'].astype(int)
+                fig = px.histogram(df_nona, 'tries', hover_data= ['question'], labels={'tries': "# of Tries"}, range_x=[-0.6,3.6], category_orders={'tries': [0,1,2,3,4]})
+                fig.update_xaxes(type='category')
+                solara.FigurePlotly(fig)
+                # solara.DataFrame(df.dropna())
+            
+        with Collapsable(header='Show Question Table'):
+            if dquest is not None:
+                solara.Markdown(f"**Question**: {dquest}")
+            solara.DataFrame(df, items_per_page=len(df), cell_actions=mc_cell_actions)
     
--- educator_dashboard/components/ResponsesComponents.py
+++ educator_dashboard/components/ResponsesComponents.py
@@ -1,64 +1,8 @@
 import solara
 
-# equivalent to from reacton import ipyvuetify as rv
-from solara.alias import rv
-from pandas import DataFrame
-
-
 from .FreeResponse import FreeResponseQuestionSingleStudent, FreeResponseSummary
 from .MultipleChoice import MultipleChoiceQuestionSingleStudent, MultipleChoiceSummary
 
-
-
-@solara.component
-def StudentQuestion(dataframe = None, roster=None, sid = None):
-    """
-    Show the responses for a single student
-    """
-    
-    if isinstance(dataframe, solara.Reactive):
-        # solara.DataFrame(df.value)
-        table = dataframe.value
-    else:
-        # solara.DataFrame(df)
-        table = dataframe
-    # table = table.to_frame()
-    table['question'] = table.index
-    
-    
-    mc_questions = table[table.tag.apply(lambda x: len(str(x).split('.'))==3)]
-
-    
-    
-    
-    # multiple choice questions
-    with solara.lab.Tabs():
-        with solara.lab.Tab("Multiple Choice"):
-            MultipleChoiceQuestionSingleStudent(mc_questions)
-            
-        with solara.lab.Tab("Free Response"):
-            FreeResponseQuestionSingleStudent(roster, sid = sid)
-        
-
-@solara.component
-def IndividualStudentResponsePanel(questions, qtags = None, sid = None):
-    
-    if sid.value is None:
-        solara.Markdown('**Select a student to see their responses**')
-        return
-    
-    
-    # squeeze dataframe to a series
-    row = questions[questions['student_id'] == sid.value].squeeze()
-    
-    # # drop student id and rename series to value
-    df = row.drop('student_id').rename('value').to_frame()
-    
-    # # add full N.tag.?(triest|score|choice) tag as a column
-    df['tag'] = qtags
-    
-    # solara.DataFrame(df)
-    StudentQuestion(df)
 
 @solara.component
 def IndividualStudentResponses(roster, sid=None):
@@ -70,33 +14,18 @@ def IndividualStudentResponses(roster, sid=None):
     if roster.value is None:
         return
     
-    # we need to replace the tag with the short version of the question
-    questions = roster.value.questions()
-    qtags = [c for c in questions.columns if '.' in c]
-    qkeys = roster.value.question_keys()
-    short_qs =  list(map(lambda x: qkeys.get(x.split('.')[1])['shorttext'] , qtags))
-    replacements = dict(zip(qtags, short_qs))
-    # questions = questions.rename(columns = replacements)
-    
-    sids = [int(s) for s in questions['student_id'].to_list()]
-    
     with solara.Card():
         if sid.value is None:
             solara.Markdown('**Select a student to see their responses**')
             return
         
-        
-        # squeeze dataframe to a series
-        row = questions[questions['student_id'] == sid.value].squeeze()
-        
-        # # drop student id and rename series to value
-        df = row.drop('student_id').rename('value').to_frame()
-        
-        # # add full N.tag.?(triest|score|choice) tag as a column
-        df['tag'] = qtags
-        
-        # solara.DataFrame(df)
-        StudentQuestion(df, roster, sid)
+        # multiple choice questions
+        with solara.lab.Tabs():
+            with solara.lab.Tab("Multiple Choice"):
+                MultipleChoiceQuestionSingleStudent(roster, sid = sid)
+                
+            with solara.lab.Tab("Free Response"):
+                FreeResponseQuestionSingleStudent(roster, sid = sid)
 
 
 @solara.component
