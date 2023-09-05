4a5dd81361998de3530ff9082b77744c45f0178c
John Arban Lewis <johnarban@users.noreply.github.com>
2023-09-05 11:37:30 -0400

    add page title

--- educator_dashboard/pages/__init__.py
+++ educator_dashboard/pages/__init__.py
@@ -20,11 +20,17 @@ def Page():
     class_id = solara.use_reactive(195) # add class id here
     roster = solara.use_reactive(cast(Roster, None))
     first_run = solara.use_reactive(True)
-
-    SetClass(class_id, roster, first_run)
     
-    Dashboard(roster) 
-    # solara.DataFrame(df.value)
+    story_name = "Hubble Data Story"
+    
+    with solara.Card():
+        #center on page
+        solara.Markdown(f"# {story_name.title()} Educator Dashboard", style={'text-align': 'center', 'width': '100%'})
+
+        SetClass(class_id, roster, first_run)
+        
+        Dashboard(roster) 
+        # solara.DataFrame(df.value)
 
 
 # The following line is required only when running the code in a Jupyter notebook:
