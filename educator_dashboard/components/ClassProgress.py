6812d7219a9360ccf20ac478866156e6910ee4ab
Pat Udomprasert <pudompra@cfa.harvard.edu>
2023-09-06 19:02:18 -0400

    Update warning text and remove "Average" Progress

--- educator_dashboard/components/ClassProgress.py
+++ educator_dashboard/components/ClassProgress.py
@@ -7,16 +7,15 @@ def ClassProgress(roster):
     
     dataframe = roster.value.short_report()
     if dataframe is None:
-        solara.Markdown("This class has a problem")
         if len(roster.value.roster) == 0:
-            solara.Markdown(f"There are no students in the class {roster.value.class_id}")
+            solara.Markdown(f"### Error: There are no students in the class {roster.value.class_id}")
             return
         else:
             with Collapsable(header = "See Class Roster to see what is wrong"):
                 solara.Markdown(f"Class roster: {roster.value.roster}")
                 return
                 
-    pavg = dataframe.percent_story_complete.median()
+    # pavg = dataframe.percent_story_complete.median()
     pmin = dataframe.percent_story_complete.min()
     pmax = dataframe.percent_story_complete.max()
-    solara.Markdown (f"## Overall Class Progress: {pavg:.0f}% (min: {pmin:.0f}%, max: {pmax:.0f}%)")
+    solara.Markdown (f"<pre> Least Progress: {pmin:.0f}%      Most Progress: {pmax:.0f}% </pre>")
