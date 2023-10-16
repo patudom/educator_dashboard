45326ed2c89a313449f767236748d16aae18e167
John Arban Lewis <johnarban@users.noreply.github.com>
2023-10-16 06:02:14 -0400

    keep tabs synced

--- educator_dashboard/components/ResponsesComponents.py
+++ educator_dashboard/components/ResponsesComponents.py
@@ -9,12 +9,12 @@ from .LayoutComponents import ScrollY
 ## TODO: split summary into stages like the inidividual student responses
 
 @solara.component
-def IndividualStudentResponses(roster, sid=None, stage_labels=[]):
+def IndividualStudentResponses(roster, sid=None, stage_labels=[], which_tab = 0):
     """
     Show response detail for each student
     sid is the currently selected student
     """
-    
+    which_tab = solara.use_reactive(which_tab)
     if isinstance(roster, solara.Reactive):
         roster = roster.value
         if roster is None:
@@ -25,8 +25,9 @@ def IndividualStudentResponses(roster, sid=None, stage_labels=[]):
         solara.Markdown(f"####  Select a student from the table above to see their responses.")
         return
     
+    
     # multiple choice questions
-    with solara.lab.Tabs():
+    with solara.lab.Tabs(value = which_tab):
         with solara.lab.Tab("Multiple Choice", classes=["horizontal-tabs"]):
             with ScrollY(height='40vh'):
                 MultipleChoiceQuestionSingleStudent(roster, sid = sid, stage_labels=stage_labels)
@@ -40,14 +41,15 @@ def IndividualStudentResponses(roster, sid=None, stage_labels=[]):
 
 
 @solara.component
-def StudentQuestionsSummary(roster, sid = None, stage_labels=[]):
-
+def StudentQuestionsSummary(roster, sid = None, stage_labels=[], which_tab = 0):
+    which_tab = solara.use_reactive(which_tab)
     if isinstance(roster, solara.Reactive):
         roster = roster.value
         if roster is None:
             return
-
-    with solara.lab.Tabs():
+    
+    
+    with solara.lab.Tabs(value = which_tab):
         with solara.lab.Tab("Multiple Choice", classes=["horizontal-tabs"]):
             # if not empty_class:
             solara.Markdown(f"####  Click any row for more detailed question information.")
