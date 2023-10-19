4cebe808ea7c7b10dfa986121f75d8d1b2e56d7f
John Arban Lewis <johnarban@users.noreply.github.com>
2023-10-19 15:39:34 -0400

    check devmode

--- educator_dashboard/components/TeacherCodeInput.py
+++ educator_dashboard/components/TeacherCodeInput.py
@@ -10,6 +10,17 @@ def TeacherCodeEntry(class_id_list, class_id, callback):
     class_id = solara.use_reactive(class_id)
     class_id_list = solara.use_reactive(class_id_list)
     proceed_to_dashboard = solara.use_reactive(False)
+    
+    dev_mode = q.in_dev_mode()
+    if dev_mode:
+        class_id_list.set([199, 200, 195, 192, 184, 188, 190, 191, 170, 172])
+        class_id.set(199)
+        proceed_to_dashboard.set(True)
+        callback()
+        # solara.Button(label="Continue to Dashboard", classes=["my-buttons"], on_click=callback, disabled=(not proceed_to_dashboard.value))
+        solara.Markdown('In dev mode, so skipping code entry')
+        return
+        
     with solara.Card(style={'position':'absolute','top':'50%', 'left':'50%', 'transform':'translate(-50%, -50%)'}, classes=["pa-16"]):
         solara.Markdown('Please enter the code provided to you by the CosmicDS team')
         with solara.Row():
